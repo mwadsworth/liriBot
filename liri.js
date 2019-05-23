b6558397c93176c03dff0d238ba8de8a04a5745f
Marilyn Wadsworth <marilyn.wadsworth86@gmail.com>
2019-05-22 18:16:09 -0600

    changed

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-//requiring varibles to call API's//
+//requiring variables to call API's//
 require("dotenv").config();
 
 
@@ -7,7 +7,7 @@ var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var request = require("request");
 var fs = require("fs");
-// var util = require("util");
+
 
 
 var spotify = new Spotify(keys.spotify);
@@ -16,8 +16,9 @@ var command = process.argv[2];
 console.log("COMMAND IS: " +command);
 var input = process.argv[3];
 console.log("INPUT IS: " +input);
-//switch case statments for liri commands//
 
+
+//switch case statments for liri commands//
 function liri(command, input) {
   switch(command) {
     case "spotify-this": 
@@ -33,6 +34,7 @@ function liri(command, input) {
 
     case "concert-this":
     //insert bandsInTown function here
+    bandsInTown(input)
     console.log("concert work?");
     break;
   }
@@ -44,52 +46,70 @@ function omdb(input) {
   var url = "http://www.omdbapi.com/?t=" + input + "&y=&plot=full&tomatoes=true&apikey=trilogy"
   axios.get(url).then(function(response){
     console.log(response.data);
-    axios.get(URL).then(function(response) {
-        // parse the response body (string) to a JSON object
-        var jsonData = response.data;
-        // showData ends up being the string containing the show data we will print to the console
-        var movieData = [
-          "Title: " + jsonData.title,
-          "Year: " + jsonData.year,
-          "Rating: " + jsonData.rating.average,
-          "Produced in: " + jsonData.country.name,
-          "Language: " + jsonData.summary,
-          "Plot: " + jsonData.plot,
-          "Actors: " + jsonData.actors
-        ].join("\r\n");
-  
-        // Append showData and the divider to log.txt, print showData to the console
-        fs.appendFile("log.txt", movieData + divider, function(err) {
-          if (err) throw err;
-          console.log(movieData);
+    console.log("The title is: " + response.data.title);
+    console.log("The year movie was released: " + response.data.year);
+    console.log("The movie's rating is: " + response.data.imdbRating);
+    console.log("The movie was produced in: " + response.data.country.name);
+    console.log("The language the movie is in: " + response.data.language);
+    console.log("The movie plot is: " + response.data.plot);
+    console.log("The actors are: " + response.data.actors);
         });
-      });
-  })
-}
+      }
 //bandsInTown
 function bandsInTown(input) {
     var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
     axios.get(url).then(function(response){
       console.log(response.data);
-      var jsonData = response.data;
-      // showData ends up being the string containing the show data we will print to the console
-      var concertData = [
-        "venueName: " + jsonData.venue,
-        "venueLocation: " + jsonData.location, //how-to- used moment.js
-        "Date: " + jsonData.date,
-      ].join("\r\n");
-
-      // Append showData and the divider to log.txt, print showData to the console
-      fs.appendFile("log.txt", concertData + divider, function(err) {
-        if (err) throw err;
-        console.log(concertData);
+      console.log("The concert is at: " + response.data.venue);
+      console.log("The location is: " + response.data.location);
+      console.log("The concert is on: " + response.data.date);
       });
-    });
-//spotify 
+    };
 
-function spotify(input) {
-    var url = ""
-}
+//spotify 
+// if (process.argv[4] === undefined || process.argv[4] === null) {
+//     var song = process.argv[3];
+//     // console.log(song);
+
+// } else if (process.argv[4] !== undefined || process.argv[4] !== null) {
+//     var song = process.argv[3] + ' ' + process.argv[4];
+//     // console.log(song);
+    
+// } else {
+//     console.log('Please enter a valid song');
+    
+// }
+
+
+// var spotify = new Spotify(keys.spotifyKeys);
+
+// // Get Spotify songs function
+// function getSpotifySong(song) {
+//     if (song === undefined || song === null) {
+//         song = 'The Sign';
+//     } else {
+        
+//     }
+
+//     spotify
+//     .search({ type: 'track', query: song, limit: 1 })
+//     .then(function(response) {
+        
+//         console.log(`####################
+//         Song: ${song} 
+        
+//         Artist: ${response.tracks.items[0].album.artists[0].name}
+        
+//         Album: ${response.tracks.items[0].album.name}
+//         Preview URL: ${response.tracks.items[0].preview_url} `);
+        
+
+
+//     })
+//     .catch(function(err) {
+//       console.log(err);
+//     });
+// };
 
 
 //calling functions
@@ -104,7 +124,3 @@ liri(command, input)
 
 
 //what each command should do//
-
-
-
-
